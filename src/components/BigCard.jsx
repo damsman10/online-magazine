df38bbc25df76fde639da907073919c69a967657
damilola <damilolaoni07@gmail.com>
2024-08-20 04:04:15 +0100

    categories completed

--- src/components/BigCard.jsx
+++ src/components/BigCard.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom"
 
 const BigCard = (props) => {
   return (
-    <div className="post flex flex-col justify-between h-[35rem] sm:w-[26rem] md:w-[30rem] lg:w-[39rem] mb-1">
-        <div className="top h-[73%] bg-contain" >
+    <div className="post flex flex-col justify-between h-[30rem] sm:w-[26rem] md:w-[30rem] lg:w-[39rem] mb-1">
+        <div className="top h-[73%]" >
         <Link to={`post/${props.slug}`}>
             <img className='object-cover' src={props.pic} alt="" /></Link>
         </div>
 
-        <Link to={`post/${props.slug}`}>
-        <div className="bottom text-center h-[27%]">
+        <Link className="h-[27%" to={`post/${props.slug}`}>
+        <div className="bottom text-center]">
             <p className='text-[0.8rem] my-3 text-[#9C9793]'>{props.category}</p>
             <h2 className='my-2 sm:text-[1.0rem] md:text-[1.8rem]'>{props.title}</h2>
             <p className='text-sm text-[#A2A591] font-bold'>{props.date}</p>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -16,13 +16,13 @@ const Category = () => {
       <AboutHero text={category.charAt(0).toUpperCase() + category.slice(1)} />
       <h1 className="text-3xl font-bold"></h1>
       <div className="w-4/5 m-auto">      
-        <div className="posts-list w-[70rem]  flex justify-between gap-8">
+        <div className="posts-list w-[70rem]  flex flex-col lg:flex-row justify-between gap-8">
           {filteredPosts.map(post => (
-            <div key={post.slug} className="post-item">
-              <img src={post.image} alt="" />
-              <h2>{post.title}</h2>
-              <p>{post.excerpt}</p>
-              <a href={`/post/${post.slug}`}>Read more</a>
+            <div key={post.slug} className="post-item w-[55%]">
+              <img className="h-[15rem]" src={post.image} alt="" />
+              <h2 className="font-bold text-xl my-2">{post.title}</h2>
+              <p className="mb-3">{post.excerpt}</p>
+              <a className="font-bold bg-[#cecece] py-2 px-5 rounded-lg" href={`/post/${post.slug}`}>Read more</a>
             </div>
           ))}
         </div>
